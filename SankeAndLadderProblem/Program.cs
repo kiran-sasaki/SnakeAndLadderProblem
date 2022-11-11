273639bdb506d271cee0c0ffdf40402de9c3ae6b
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-11 16:32:37 +0530

    Added Welcome Message For SnakeAndLadderProblem

--- SankeAndLadderProblem/Program.cs
+++ SankeAndLadderProblem/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SnakeAndLadderProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder problem");
+        }
+    }
+}
