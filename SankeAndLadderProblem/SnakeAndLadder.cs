739ddd9428606f2668225f950ffd74a7e9acc8a0
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-12 15:51:48 +0530

    Added If Statement For SnakeAndLadderProblem

--- SankeAndLadderProblem/SnakeAndLadder.cs
+++ SankeAndLadderProblem/SnakeAndLadder.cs
@@ -11,34 +11,43 @@ namespace SnakeAndLadderProblem
         public const int LADDER = 1;
         public const int SNAKE = 2;
         public const int NO_PLAY = 0;
-        public const int EndPosition = 100;
+        public const int WINPOSITION = 100;
         public static void SnakeAndLadderGame()
         {
-            int StartPosition = 0;
             int PlayerPosition =0;
-            Console.WriteLine("Started with " + StartPosition + " position");
-            //UC2
-            Random Dice = new Random();
-            int diceRoll = Dice.Next(1, 7);
-            Console.WriteLine("Dice Roll " + diceRoll);
-            //UC3
-            int option = Dice.Next(0, 3);
-            switch (option)
+            //UC4
+            while (PlayerPosition < WINPOSITION)
             {
-                case 0:
-                    Console.WriteLine("No Snake No Ladder");
-                    Console.WriteLine("Your Position is : " + PlayerPosition);
-                    break;
-                case 1:
-                    PlayerPosition += diceRoll;
-                    Console.WriteLine("You Got Ladder");
-                    Console.WriteLine("Your Position After Ladder is : " + PlayerPosition);
-                    break;
-                case 2:
-                    PlayerPosition -= diceRoll;
-                    Console.WriteLine("You Got Snake");
-                    Console.WriteLine("Your Position After Snake is : " + PlayerPosition);
-                    break;
+                //UC2
+                Random Dice = new Random();
+                int diceRoll = Dice.Next(1, 7);
+                Console.WriteLine("Dice Roll " + diceRoll);
+                int option = Dice.Next(0, 3);
+                //UC3
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Snake No Ladder");
+                        Console.WriteLine("Your Position is : " + PlayerPosition);
+                        break;
+                    case 1:
+                        PlayerPosition += diceRoll;
+                        Console.WriteLine("You Got Ladder");
+                        Console.WriteLine("Your Position After Ladder is : " + PlayerPosition);
+                        break;
+                    case 2:
+                        if (PlayerPosition < 0)
+                        {
+                            PlayerPosition = 0;
+                        }
+                        else
+                        {
+                            PlayerPosition -= diceRoll;
+                        }
+                        Console.WriteLine("You Got Snake");
+                        Console.WriteLine("Your Position After Snake is : " + PlayerPosition);
+                        break;
+                }
             }
         }
     }
