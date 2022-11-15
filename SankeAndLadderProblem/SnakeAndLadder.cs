bf57aaed4303b3963bc7c7c5b98cf63033df1380
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-15 08:35:56 +0530

    Added Two Players For SnakeAndLadderProblem

--- SankeAndLadderProblem/SnakeAndLadder.cs
+++ SankeAndLadderProblem/SnakeAndLadder.cs
@@ -14,47 +14,105 @@ namespace SnakeAndLadderProblem
         public const int WINPOSITION = 100;
         public static void SnakeAndLadderGame()
         {
-            int PlayerPosition =0;
+            int Player1 =0;
+            int Player2 =0;
+            int CurrentPlayer = 1;
             int DieCount = 0;
             //UC4
-            while (PlayerPosition < WINPOSITION)
+            while (Player1 < WINPOSITION && Player2 < WINPOSITION)
             {
                 //UC2
                 Random Dice = new Random();
                 int diceRoll = Dice.Next(1, 7);
-                Console.WriteLine("Dice Roll " + diceRoll);
                 int option = Dice.Next(0, 3);
                 //UC3
                 switch (option)
                 {
-                    case 0:
-                        Console.WriteLine("No Snake No Ladder");
-                        Console.WriteLine("Your Position is : " + PlayerPosition);
-                        break;
-                    case 1:
-                        PlayerPosition += diceRoll;
-                        //UC5
-                        if (PlayerPosition > WINPOSITION)
+                    case NO_PLAY:
+                        if (CurrentPlayer == 1)
                         {
-                            PlayerPosition -= diceRoll;
-                            Console.WriteLine("Stay in some previous position " + PlayerPosition);
-                        }
-                        Console.WriteLine("You Got Ladder");
-                        Console.WriteLine("Your Position After Ladder is : " + PlayerPosition);
-                        break;
-                    case 2:
-                        if (PlayerPosition < 0)
-                        {
-                            PlayerPosition = 0;
+                            Console.WriteLine("No Play");
+                            Console.WriteLine(" Position of Player1 is : " + Player1);
                         }
                         else
                         {
-                            PlayerPosition -= diceRoll;
+                            Console.WriteLine("No Play");
+                            Console.WriteLine(" Position of Player2 is : " + Player2);
                         }
-                        Console.WriteLine("You Got Snake");
-                        Console.WriteLine("Your Position After Snake is : " + PlayerPosition);
                         break;
-                        DieCount++;
+                    case LADDER:
+                        if (CurrentPlayer == 1)
+                        {
+                            if (Player1 < WINPOSITION)
+                            {
+                                Player1 += diceRoll;
+                                //UC5
+                                if (Player1 > WINPOSITION)
+                                {
+                                    Player1 -= diceRoll;
+                                    Console.WriteLine("Stay in the previous position for Player1 " + Player1);
+                                }
+                                Console.WriteLine("You Got Ladder");
+                                Console.WriteLine("Position of Player1 After Ladder: " + Player1);
+                                CurrentPlayer = 1;
+                                if (Player1 == WINPOSITION)
+                                {
+                                    Console.WriteLine("Player1 Has Won The Game");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (Player2 < WINPOSITION)
+                            {
+                                Player2 += diceRoll;
+                                //UC5
+                                if (Player2 > WINPOSITION)
+                                {
+                                    Player2 -= diceRoll;
+                                    Console.WriteLine("Stay in the previous position for Player2 " + Player2);
+                                }
+                                Console.WriteLine("You Got Ladder");
+                                Console.WriteLine("Position of Player2 After Ladder: " + Player2);
+                                CurrentPlayer = 2;
+                                if (Player2 == WINPOSITION)
+                                {
+                                    Console.WriteLine("Player2 Has Won The Game");
+                                }
+                            }
+                        }
+                        break;
+
+                    case SNAKE:
+                        if (CurrentPlayer == 1)
+                        {
+                            if (Player1 < WINPOSITION)
+                            {
+                                Player1 -= diceRoll;
+                                if (Player1 < 0)
+                                {
+                                    Player1 = 0;
+                                }
+                                CurrentPlayer = 2;
+                                Console.WriteLine("You Got Snake");
+                                Console.WriteLine("Position of Player1 is: " + Player1);
+                            }
+                        }
+                        else
+                        {
+                            if (Player2 < WINPOSITION)
+                            {
+                                Player2 -= diceRoll;
+                                if (Player2 < 0)
+                                {
+                                    Player2 = 0;
+                                }
+                                CurrentPlayer = 1;
+                                Console.WriteLine("You Got Snake");
+                                Console.WriteLine("Position of Player2 is: " + Player2);
+                            }
+                        }
+                        break;
                 }
                 //UC6
                 DieCount++;
